d9439cad6ad8e1029cfe970bde6b481362aa8bbf
Boobalan-30 <boobalandisk30072001@gmail.com>
2025-08-29 18:30:28 +0530

    main-branch

--- user-service/src/main/java/com/onlinecourse/userservice/exception/customexception/EducationNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.onlinecourse.userservice.exception.customexception;
-
-public class EducationNotFoundException extends RuntimeException {
-    public EducationNotFoundException(String message) {
-        super(message);
-    }
-}
