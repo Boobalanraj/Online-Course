2fe606ad9d59b0850d62fa83344a8d1adc77d5b3
Boobalan-30 <boobalandisk30072001@gmail.com>
2025-09-01 18:01:32 +0530

    update Eureka-Setup

--- API_Gateway-Service/src/main/java/com/onlinecourse/apigatewayservice/ApiGatewayServiceApplication.java
+++ API_Gateway-Service/src/main/java/com/onlinecourse/apigatewayservice/ApiGatewayServiceApplication.java
@@ -1,4 +1,4 @@
-package com.onlinecourse.userservice;
+package com.onlinecourse.apigatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class UserServiceApplication {
+public class ApiGatewayServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
+		SpringApplication.run(ApiGatewayServiceApplication.class, args);
 	}
 
 }
